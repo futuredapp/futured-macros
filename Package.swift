be0b2e773d2d9dd33068e55136a0494eeb0d1cea
Mikoláš Stuchlík <mikolas.stuchlik@futured.app>
2025-01-14 12:51:05 +0100

    Update Package manifest

--- Package.swift
+++ Package.swift
@@ -8,9 +8,13 @@ let package = Package(
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         .library(
-            name: "FuturedMacros",
+            name: "FuturedMacrosEnum",
             targets: ["EnumIdentable"]
-        )
+        ),
+        .library(
+            name: "FuturedMacrosCache",
+            targets: ["DataCache"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
@@ -34,6 +38,7 @@ let package = Package(
 
         // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "EnumIdentable", dependencies: ["EnumIdentableMacros"]),
+        .target(name: "DataCache", dependencies: ["DataCacheMacros"]),
 
         // A test target used to develop the macro implementation.
         .testTarget(
